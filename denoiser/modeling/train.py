eda27dcf80784aaa5d1fb5504508a40d895316de
Mateusz Krol <matkrol13@gmail.com>
2025-10-09 15:11:51 +0200

    Working workaround with sequence loader from Keras

--- denoiser/modeling/train.py
+++ denoiser/modeling/train.py
@@ -1,21 +1,30 @@
 from tensorflow.keras.callbacks import ReduceLROnPlateau, TensorBoard
-import h5py
 import numpy as np
+import pickle
 
-from denoiser.config import TRAIN_DATA, REPORTS_DIR, MODELS_DIR
+from denoiser.config import DATA_DIR, REPORTS_DIR, MODELS_DIR
+from models import SequenceLoader
 from models.custom_models import cnn_ced
 from models.custom_callbacks import LearningRateStopping, LearningRateLogger
 
 if __name__ == "__main__":
 
     # Load data
-    f = h5py.File(name=TRAIN_DATA / 'source.mat', mode='r')
-    f2 = h5py.File(name=TRAIN_DATA / 'target.mat', mode='r')
-    train_x = np.transpose(np.array(f.get('sourceTrain')), axes=[0, 2, 1])
-    train_y = np.array(f2.get('targetTrain'))
+    with open(DATA_DIR / 'clean.pkl', "rb") as f:
+        y = pickle.load(f)
+    with open(DATA_DIR / 'noisy.pkl', "rb") as f:
+        x = pickle.load(f)
+
+    # dirty dataload workaround to see if everything works
+    train = SequenceLoader(x_noisy=x, y_clean=y, window_size=8, batch_size=128, shuffle=True)
+    validate = SequenceLoader(x_noisy=x, y_clean=y, window_size=8, batch_size=128, shuffle=True)
+    # around 5% used for validation
+    split_idx = len(train.indexes) // 20
+    train.indexes, validate.indexes = train.indexes[split_idx:], train.indexes[:split_idx]
+
 
     # Model
-    model = cnn_ced(model_name="CNN-CED-1200", show_model=False)
+    model = cnn_ced(model_name="CNN-CED-2000", show_model=False)
 
     callbacks = [
         ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=4, verbose=1, min_lr=1e-4),
@@ -24,5 +33,5 @@ if __name__ == "__main__":
         LearningRateStopping(min_lr=1e-4),
     ]
 
-    result = model.fit(train_x, train_y, validation_split=0.05, batch_size=128, epochs=120, verbose=1, callbacks=callbacks)
+    result = model.fit(x=train, validation_data=validate, epochs=120, verbose=1, callbacks=callbacks)
     model.save(f"{MODELS_DIR / model.name}.keras")
