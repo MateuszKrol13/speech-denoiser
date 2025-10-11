dd056b5dd072b22b16ed50d23483d5300336d6b0
Mateusz Krol <matkrol13@gmail.com>
2025-10-11 22:45:29 +0200

    Converted process_data script to a function, for model inference use. Moved constants to denoiser.config

--- denoiser/config.py
+++ denoiser/config.py
@@ -2,18 +2,24 @@ from pathlib import Path
 from numpy import mean, std
 
 # Paths
-PROJ_ROOT = Path(__file__).resolve().parents[1]
+PROJ_ROOT = Path(__file__).resolve().parents[1]  # project root directory
 DATA_DIR = PROJ_ROOT / "data\\processed\\2025-10-11_16-13\\"
+RAW_DATA = PROJ_ROOT / "data\\raw\\cv-corpus-20.0-delta-2024-12-06\\en\\clips\\"
+
 CLEAN_DATA = DATA_DIR / "clean.pkl"
 NOISY_DATA = DATA_DIR / "noisy.pkl"
 DATA_METADATA = DATA_DIR / "metadata.pkl"
 METADATA_READABLE = DATA_DIR / "metadata.txt"
 
-
 MODELS_DIR = PROJ_ROOT / "models"
-
 REPORTS_DIR = PROJ_ROOT / "reports"
 FIGURES_DIR = REPORTS_DIR / "figures"
+
+# denoiser parameters
+SNR_LEVEL = 0  # dB
+SAMPLING_FREQ = 8000  # Hz
+FRAMES_LENGTH = 8  # Count of frames used to reconstruct clean signal
+NOISE = 'PinkNoise'
 
 # Data statistics
 DATA_STATS = {
--- preprocessing/process_data.py
+++ preprocessing/process_data.py
@@ -5,36 +5,16 @@ Output data will a 2D ndarray(shape=(features, window_count)) that contains conc
 transform. Each index of noisy data corrseponds to each index of clean data.
 """
 import json
-
-SNR_LEVEL = 0
-TARGET_FREQ = 8000
-FRAMES_LENGTH = 8
-NOISE = 'PinkNoise'
-OUTPUT_DATA = 'Spectrogram absolute values'
-
-TRAIN_SPLIT = 0.97
-TEST_SPLIT = 1 - TRAIN_SPLIT
-REC_NUMBER = 4000  # To be deleted later on.
-
 import os
 import numpy as np
 from time import perf_counter
 import librosa
 from datetime import datetime
 import pickle
+from typing import Union
 
-from denoiser.config import DATA_METADATA, DATA_STATS, METADATA_READABLE
+from denoiser.config import DATA_STATS, RAW_DATA, SNR_LEVEL
 
-def conv_to_dataset(jagged_ndarray, noisy=False):
-    output = []
-    for spectrogram in jagged_ndarray:
-        features, frame = spectrogram.shape
-        assert features == 129
-        for f in range(frame-8+1):
-            add_frame = spectrogram[:, f] if not noisy else spectrogram[:, f:f + 8]  # Magic number to be described -> from moving window
-            output.append(add_frame)
-
-    return np.stack(output, axis=0)
 
 def generate_pink_noise(n_samples:int, sample_rate:int):
     white_noise = np.random.randn(n_samples)
@@ -45,7 +25,6 @@ def generate_pink_noise(n_samples:int, sample_rate:int):
     pink_fft = white_fft * scale
     pink_noise = np.fft.irfft(pink_fft, n_samples)
     return pink_noise
-
 
 def apply_noise(clear_signal, noise_signal, *, snr_level):
     if not clear_signal.shape == noise_signal.shape:
@@ -61,30 +40,58 @@ def apply_noise(clear_signal, noise_signal, *, snr_level):
     return scaled_noise + clear_signal
 
 def normalize_audio(signal):
+    """Normalises audio to [-1;1] value range. Used to limit the amplitude of audio after applying noise.
+    :param signal: Audio signal (1D-ndarray)
+    :return: same audio signal normalised.
+    """
     scaling_factor = max(abs(signal))
     if scaling_factor == 0:  # empty recordings
         return signal
     else:
         return signal / max(abs(signal))
 
-if "__main__" == __name__:
+def prepare_data(
+        include_phase: bool=True,
+        path: str=None,
+        rec_no: int=None,
+        snr_level: Union[int, float]=SNR_LEVEL,
+) -> None:
+    """Converts directory containing mp3 recordings into list of ndarrays containing specrogram data of noisy and clear
+    recordings and saves them as pickle file if path is not specified.
 
-    DIRECTORY = "..\\data\\raw\\cv-corpus-20.0-delta-2024-12-06\\en\\clips"
+    :param include_phase: saves phase from noisy spectrogram
+    :param path: if path is specified, provided path is used instead of paths specified in denoiser.config. Processed
+    data is then saved back to the same directory with corresponding name of .mp3 file. Please do note, that if feature
+    extraction fails, there will be a shift in file name, so it's not error safe. Do watch the terminal for errors when
+    preparing data for inference. For preparing training dataset this is not an issue.
+    :param rec_no: specifies the number of recordings used inside provided (or set in denoiser.config) directory.
+    if set to None, every .mp3 file inside directory is used.
+    :param snr_level: sets signal-to-noise rato in dB value for noisy recording. Regardless of specified SNR, noisy
+    audio is limited to [-1, 1] range. Defaults to value specified in denoiser.config.
+    :return: Saves data as bytestream of list of spectrogram data (2D-ndarray) or if path is specified, then each .mp3
+    file will get corresponding .npy file containing noisy and clean audio spectrogram magnitude and phase data of noisy
+    recording, used in audio reconstruction
+    """
+
     # load audio and prepare data containers
-    audio_files = [os.path.join(DIRECTORY, f) for f in os.listdir(DIRECTORY) if f.endswith(".mp3")]
-    clear_input = []
-    noisy_input = []
+    if path is None:
+        audio_files = [os.path.join(RAW_DATA, f) for f in os.listdir(RAW_DATA) if f.endswith(".mp3")]
+    else:
+        audio_files = [os.path.join(path, f) for f in os.listdir(path) if f.endswith(".mp3")]
+    clean_data_magnitude = []
+    noisy_data_magnitude = []
+    phase_data = []  # Phase data from noisy recording can be used to deconstruct spectrogram into audio
 
     # Process loop
-    print(f"Loading and rocessing {min(REC_NUMBER, len(audio_files))} recordings...")
+    print(f"Loading and rocessing {min(rec_no, len(audio_files))} recordings...")
     timer_start = perf_counter()
-    for recording_no in range(min(REC_NUMBER, len(audio_files))):
+    for recording_no in range(min(rec_no, len(audio_files))):
         # Progress bar
-        remaining_time = perf_counter() * (REC_NUMBER - recording_no + 1) / (
+        remaining_time = perf_counter() * (rec_no - recording_no + 1) / (
                     recording_no + 1)  # Estimate remaining by measuring elapsed
         elapsed = perf_counter() - timer_start
         print(
-            f"Loading completed in {(recording_no / REC_NUMBER) * 100:.0f}%"
+            f"Loading completed in {(recording_no / rec_no) * 100:.0f}%"
             f"\t\telapsed ({elapsed // 60:.0f}min, {elapsed % 60:.0f}s)"
             f"\t\testimated remaining ({remaining_time // 60:.0f}min, {remaining_time % 60:.0f}s)",
             end="\r"
@@ -94,9 +101,9 @@ if "__main__" == __name__:
         try:
             # get clean and noisy audio
             clean_signal, sr = librosa.load(audio_files[recording_no])
-            downsampled_signal = librosa.resample(clean_signal, orig_sr=sr, target_sr=TARGET_FREQ, res_type='scipy')
-            pink_noise = generate_pink_noise(downsampled_signal.shape[0], TARGET_FREQ)  # called each time, cause we want "random" noise
-            noisy_signal = normalize_audio(apply_noise(downsampled_signal, pink_noise, snr_level=SNR_LEVEL))
+            downsampled_signal = librosa.resample(clean_signal, orig_sr=sr, target_sr=SAMPLING_FREQ, res_type='scipy')
+            pink_noise = generate_pink_noise(downsampled_signal.shape[0], SAMPLING_FREQ)  # called each time, cause we want "random" noise
+            noisy_signal = normalize_audio(apply_noise(downsampled_signal, pink_noise, snr_level=snr_level))
 
             # derive spectrograms
             clean_spectrogram = librosa.stft(downsampled_signal, n_fft=256, hop_length=64, win_length=256, window="hamming")
@@ -104,8 +111,11 @@ if "__main__" == __name__:
 
             # eight noisy consecutive frames are used to reconstruct first frame
             assert clean_spectrogram.shape == noisy_spectrogram.shape
-            clear_input.append(abs(clean_spectrogram).astype('float32'))
-            noisy_input.append(abs(noisy_spectrogram).astype('float32'))
+            clean_data_magnitude.append(abs(clean_spectrogram).astype('float32'))
+            noisy_data_magnitude.append(abs(noisy_spectrogram).astype('float32'))
+
+            if include_phase:
+                phase_data.append(np.angle(noisy_spectrogram).astype('float32'))
 
         except ValueError as e:
             print("Concatenation failed, terminating dataset generation!")
@@ -120,27 +130,38 @@ if "__main__" == __name__:
             print("Exception happened, continuing...", e, end='\n')
 
     print(f"\nProcessing finished!\nNormalising data...")
-    savefile = os.path.join("..\\data\\processed\\", datetime.now().strftime("%Y-%m-%d_%H-%M"))
-    os.mkdir(savefile)
-    clean_data, noisy_data = os.path.join(savefile, "clean.pkl"), os.path.join(savefile, "noisy.pkl")
+    savefile = os.path.join("..\\data\\processed\\", datetime.now().strftime("%Y-%m-%d_%H-%M")) if path is None else path
     metadata_ = {}
 
-    for arr, save in zip((clear_input, noisy_input), (clean_data, noisy_data)):
-        data_type = os.path.basename(save).split('.')[0]
-        print(f"Processing {data_type} data...", end="\t")
-        flatten_data = np.concatenate([a.flatten() for a in arr])
+    if path is None:  # Dataset preparation
+        os.mkdir(savefile)
+        clean_data, noisy_data = os.path.join(savefile, "clean.pkl"), os.path.join(savefile, "noisy.pkl")
 
-        for stat, func in DATA_STATS.items():
-            stat_name = "_".join([data_type, stat])
-            metadata_[stat_name] = func(flatten_data)
+        for arr, save in zip((clean_data_magnitude, noisy_data_magnitude), (clean_data, noisy_data)):
+            data_type = os.path.basename(save).split('.')[0]
+            print(f"Processing {data_type} data...", end="\t")
+            flatten_data = np.concatenate([a.flatten() for a in arr])
 
-        mean, std = metadata_[data_type + "_mean"], metadata_[data_type + "_std"]
-        norm_arr = [(a - mean) / std for a in arr]
+            for stat, func in DATA_STATS.items():
+                stat_name = "_".join([data_type, stat])
+                metadata_[stat_name] = func(flatten_data)
 
-        print("saving...", end="\t")
-        with open(save, "wb") as file:
-            pickle.dump(norm_arr, file)
-        print("Done!")
+            mean, std = metadata_[data_type + "_mean"], metadata_[data_type + "_std"]
+            norm_arr = [(a - mean) / std for a in arr]
+
+            print("saving...", end="\t")
+            with open(save, "wb") as file:
+                pickle.dump(norm_arr, file)
+            print("Done!")
+    else:
+        for f_idx in range(len(audio_files)):
+            file_name = os.path.basename(audio_files[f_idx]).split('.')[0]
+            for arr, save in zip(
+                    (clean_data_magnitude[f_idx], noisy_data_magnitude[f_idx], phase_data[f_idx]),
+                    ("_clean_mag.npy", "_noisy_mag.npy", "_phase.npy")
+            ):
+                with open(os.path.join(file_name, save), "wb") as fs:
+                    np.save(fs, arr)
 
     print("Saving metadata...", end="\n")
     with open(os.path.join(savefile, "metadata.pkl"), 'wb') as file:  # binary form as json lib does not like numpy types
@@ -151,3 +172,6 @@ if "__main__" == __name__:
         json.dump({k:float(v) for k, v in metadata_.items()}, readable_file)
 
     print("Data processing finished!")
+
+if "__main__" == __name__:
+    prepare_data(include_phase=False, rec_no=2000)
