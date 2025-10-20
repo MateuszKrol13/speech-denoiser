Fixed noise generation - seriously this time, and added z-normalisation to inference script

denoiser/modeling/predict.py
@@ -3,8 +3,9 @@ import numpy as np
 from numpy.typing import NDArray
 from typing import Union
 import os
+import pickle
 
-from denoiser.config import MODELS_DIR, FRAMES_LENGTH
+from denoiser.config import MODELS_DIR, FRAMES_LENGTH, DATA_METADATA
 
 def predict(
         model: Union[keras.models.Model, keras.models.Sequential, str],
@@ -13,6 +14,9 @@ def predict(
 
     if not os.path.exists(audio_path):
         raise FileNotFoundError("Specified directory does not exist!")
+
+    with open(DATA_METADATA, "rb") as f:
+        metadata_ = pickle.load(f)
 
     if isinstance(model, (keras.models.Model, keras.models.Sequential)):
         loaded_model = model
@@ -26,9 +30,12 @@ def predict(
     for file_path in spec_paths:
         with open(file_path, "rb") as f:
             spec_magnitude = np.load(f).T  # data shape needs to be (..., 129) and is saved the other way around
-        windowed = np.lib.stride_tricks.sliding_window_view(spec_magnitude, window_shape=FRAMES_LENGTH, axis=0)
+        spec_normalised = (spec_magnitude - metadata_["noisy_mean"]) / metadata_["noisy_std"]
+        windowed = np.lib.stride_tricks.sliding_window_view(spec_normalised, window_shape=FRAMES_LENGTH, axis=0)
 
-        predictions.append(loaded_model.predict(windowed, verbose="auto", steps=None, callbacks=None))
+        pred = loaded_model.predict(windowed, verbose="auto", steps=None, callbacks=None)
+        pred_norm = (np.squeeze(pred).T * metadata_["clean_std"]) + metadata_["clean_mean"]  # (..., 129, 1) -> (129, ...)
+        predictions.append(pred_norm)
 
     return predictions if predictions else None
 

denoiser/process_data.py
@@ -73,21 +73,22 @@ def apply_noise(clear_signal, noise_signal, *, snr_level):
     with N = factor * n:
     P_N = 1 / len(N) * sum( (factor * sample)^2 for sample in n)
     P_N = factor^2 / len(N) * sum(sample^2 for sample in n)
-    factor = sqrt( P_N * len(n) * sum(sample^2 for sample in n) )
-    factor = sqrt(P_N * len(n)) * sqrt(sum(sample^2 for sample in n))
+    factor = sqrt( P_N * len(n) / sum(sample^2 for sample in n) )
+    factor = sqrt(P_N * len(n)) / sqrt(sum(sample^2 for sample in n))
     
     substituting P_N from SNR_db
-    factor = sqrt( P_S / 10^(SNR_db / 10) * len(n)) * sqrt(sum(sample^2 for sample in n))
+    factor = sqrt( P_S / 10^(SNR_db / 10) * len(n)) / sqrt(sum(sample^2 for sample in n))
     
     with P_S ~ RMS_S ^ 2 = 1 / len(S) * sum( sample^2 for sample in S)
-    factor = sqrt( 1 / 10^(SNR_db / 10) ) * sum(sample^2 for sample in n) / sum(sample^2 for sample in S)
+    factor = sqrt( 1 / 10^(SNR_db / 10) ) * sqrt ( sum(sample^2 for sample in S) / sum(sample^2 for sample in n) )
     """
     signal_sum = np.sum(np.square(clear_signal))
+    noise_sum = np.sum(np.square(noise_signal))
     if signal_sum == 0:
         return noise_signal
 
     snr_factor = np.sqrt(1 / 10 ** (snr_level / 10))
-    noise_factor = snr_factor * np.sum(np.square(noise_signal)) / signal_sum
+    noise_factor = snr_factor * np.sqrt(signal_sum / noise_sum)
     scaled_noise = noise_factor * noise_signal
 
     return scaled_noise + clear_signal